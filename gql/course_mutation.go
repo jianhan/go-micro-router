b50ba697735ffdb16eee2e26c7b54a18896742fe
Jian Han <jianhan@mail.com>
2018-04-02 16:25:54 +1000

    [DEV] Mutation functional now

--- gql/course_mutation.go
+++ gql/course_mutation.go
@@ -1,6 +1,8 @@
 package gql
 
 import (
+	"context"
+
 	"github.com/gosimple/slug"
 	"github.com/graphql-go/graphql"
 	pcourse "github.com/jianhan/go-micro-courses/proto/course"
@@ -13,7 +15,13 @@ func createCourse(coursesClient pcourse.CourseServiceClient) *graphql.Field {
 		Type:        courseType,
 		Description: "Create new course",
 		Args: graphql.FieldConfigArgument{
+			"id": &graphql.ArgumentConfig{
+				Type: graphql.String,
+			},
 			"name": &graphql.ArgumentConfig{
+				Type: graphql.NewNonNull(graphql.String),
+			},
+			"slug": &graphql.ArgumentConfig{
 				Type: graphql.NewNonNull(graphql.String),
 			},
 			"display_order": &graphql.ArgumentConfig{
@@ -33,13 +41,27 @@ func createCourse(coursesClient pcourse.CourseServiceClient) *graphql.Field {
 			},
 		},
 		Resolve: func(params graphql.ResolveParams) (interface{}, error) {
+			id, _ := params.Args["id"].(string)
 			name, _ := params.Args["name"].(string)
 			slug := slug.Make(name)
 			displayOrder, _ := params.Args["display_order"].(int)
 			start, _ := params.Args["start"].(string)
 			end, _ := params.Args["end"].(string)
 			q.Q(name, slug, displayOrder, start, end)
-			return nil, nil
+			rsp, err := coursesClient.UpsertCourse(
+				context.Background(),
+				&pcourse.UpsertCourseReq{
+					Course: &pcourse.Course{
+						ID:   id,
+						Name: name,
+						Slug: slug,
+					},
+				},
+			)
+			if err != nil {
+				return nil, err
+			}
+			return rsp.Course, nil
 
 		},
 	}
