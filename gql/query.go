b995d62d3c60aa74ebe465cd4a5bd70db27997c3
Jian Han <jianhan@mail.com>
2018-04-02 10:14:06 +1000

    [DEV] Updated course query generator

--- gql/query.go
+++ gql/query.go
@@ -2,15 +2,26 @@ package gql
 
 import (
 	"github.com/graphql-go/graphql"
-	"github.com/jianhan/go-micro-courses/proto/course"
+	pcourse "github.com/jianhan/go-micro-courses/proto/course"
 )
-
-var QueryMap = map[string]*graphql.Field{}
-
-func init() {
-	QueryMap["courses"] = getCourseQuery(course.NewCoursesClient("", nil))
-}
 
 type QueryMutationGenerator interface {
 	Generate() map[string]*graphql.Field
+}
+
+type queryGenerator struct {
+	coursesClient pcourse.CoursesClient
+}
+
+func NewQueryGenerator(coursesClient pcourse.CoursesClient) QueryMutationGenerator {
+	return &queryGenerator{
+		coursesClient: coursesClient,
+	}
+}
+
+func (q *queryGenerator) Generate() map[string]*graphql.Field {
+	return map[string]*graphql.Field{
+		// "courses": getCourseQuery(course.NewCoursesClient("", nil)),
+		"courses": getCourseQuery(q.coursesClient),
+	}
 }
